748d06bb338b7dacd65d2099fb14b0fbaed6dfe3
Philip Andreas <andreas.philip97@gmail.com>
2021-02-13 18:16:13 +0700

    Add about section

---
+++ scripts/constants.js
@@ -0,0 +1,11 @@
+export default {
+    ANIMATE_INTRO: 'animate-intro',
+    ANIMATE_ABOUT: 'animate-about',
+    ANIMATE_EXPERIENCE: 'animate-experience',
+
+    ACTIVE_STORY: 'active-story',
+    SELECTED_STORY: 'selected-story',
+
+    ABOUT_CONTAINER: 'about-container',
+    EXPERIENCE_CONTAINER: 'experience-container'
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,71 @@
 import anime from './anime.es.js';
+import constants from './constants.js';
 
 $(document).ready(_ => {
+    let containerShown = {};
+    let containerClass = {};
+    containerClass[constants.ABOUT_CONTAINER] = constants.ANIMATE_ABOUT;
+    containerClass[constants.EXPERIENCE_CONTAINER] = constants.ANIMATE_EXPERIENCE;
+
     function init() {
         initAnimations();
-        initActionEvents();
+        initActionItems();
     }
 
     function initAnimations() {
         anime({
-            targets: document.getElementsByClassName('animate-intro'),
-            translateX: [-50, 0],
+            targets: document.getElementsByClassName(constants.ANIMATE_INTRO),
+            translateY: [25, 0],
             opacity: 1,
             easing: 'linear',
             duration: 500,
             delay: anime.stagger(300, {start: 200})
         });
 
-        anime({
-            targets: document.getElementsByClassName('arrow-intro'),
-            keyframes: [
-                {translateY: 12},
-                {translateY: 0},
-            ],
-            loop: true,
-            easing: 'linear',
-            duration: 1000
+        let observer = new IntersectionObserver(function (entries, _) {
+            entries.forEach(entry => {
+                if (!containerShown[entry.target.id] && entry.isIntersecting) {
+                    console.log(entry.target.id);
+                    anime({
+                        targets: document.getElementsByClassName(containerClass[entry.target.id]),
+                        opacity: 1,
+                        easing: 'linear',
+                        duration: 500,
+                        delay: anime.stagger(300, {start: 200})
+                    });
+                }
+            });
+        }, {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
         });
+
+        observer.observe(document.getElementById(constants.ABOUT_CONTAINER));
+        // observer.observe(document.getElementById(constants.EXPERIENCE_CONTAINER));
     }
 
-    function initActionEvents() {
-        // TODO add on scroll event to show and hide #scroll-arrow
+    function initActionItems() {
+        let experienceBlibli = $('#experience-blibli');
+        let experienceBinus = $('#experience-binus');
 
-        $('#scroll-arrow').click(_ => {
-            // TODO scroll down
+        let blibliStory = $('#blibli-story');
+        let binusStory = $('#binus-story');
+
+        experienceBlibli.click(() => {
+            experienceBinus.removeClass(constants.SELECTED_STORY);
+            experienceBlibli.addClass(constants.SELECTED_STORY);
+
+            binusStory.removeClass(constants.ACTIVE_STORY);
+            blibliStory.addClass(constants.ACTIVE_STORY);
+        });
+
+        experienceBinus.click(() => {
+            experienceBlibli.removeClass(constants.SELECTED_STORY);
+            experienceBinus.addClass(constants.SELECTED_STORY);
+
+            blibliStory.removeClass(constants.ACTIVE_STORY);
+            binusStory.addClass(constants.ACTIVE_STORY);
         });
     }
 
